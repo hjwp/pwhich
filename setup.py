a5fa265e2bb9b8d6ea0e0066df5f161134155f03
Harry <hjwp2@cantab.net>
2011-12-07 08:52:31 +0000

    try to write a distutils/pypi setup.py script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pwhich',
+    version='0.1',
+    description='Python Which',
+    author='Harry Percival',
+    author_email='hjwp2@cantab.net',
+    url='http://harry.pythonanywhere.com/static/pwhich/',
+    packages=[],
+    scripts='scripts/pwhich.py',
+ )
+

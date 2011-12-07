2b2373b02c4a9ce0bb7c1c6f906af6048ffa2360
Harry <hjwp2@cantab.net>
2011-12-07 15:40:21 +0000

    update metadata

--- setup.py
+++ setup.py
@@ -8,8 +8,15 @@ setup(
     description='Python which - find out where Python modules are located on your drive',
     author='Harry Percival',
     author_email='hjwp2@cantab.net',
-    url='https://github.com/hjwp/pwhich/downloads',
+    url='https://github.com/hjwp/pwhich',
+    download_url='https://github.com/hjwp/pwhich/blob/master/dist/pwhich-0.1.0.linux-x86_64.zip',
     packages=[],
     scripts=['scripts/pwhich.py'],
- )
+    classifiers= [
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+)
 
